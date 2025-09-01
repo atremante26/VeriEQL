4f3c371de59661a03bb67585ffbbee72d3e95b97
whatsmyname <yanghece96@gmail.com>
2025-09-01 13:52:50 -0700

    update z3.py

--- z3py_libs/z3.py
+++ z3py_libs/z3.py
@@ -1220,14 +1220,14 @@ def _coerce_exprs(a, b, ctx=None):
     if not is_expr(a) and not is_expr(b):
         a = _py2expr(a, ctx)
         b = _py2expr(b, ctx)
-    if isinstance(a, str) and isinstance(b, SeqRef):
-        a = StringVal(a, b.ctx)
-    if isinstance(b, str) and isinstance(a, SeqRef):
-        b = StringVal(b, a.ctx)
-    if isinstance(a, float) and isinstance(b, ArithRef):
-        a = RealVal(a, b.ctx)
-    if isinstance(b, float) and isinstance(a, ArithRef):
-        b = RealVal(b, a.ctx)
+    # if isinstance(a, str) and isinstance(b, SeqRef):
+    #     a = StringVal(a, b.ctx)
+    # if isinstance(b, str) and isinstance(a, SeqRef):
+    #     b = StringVal(b, a.ctx)
+    # if isinstance(a, float) and isinstance(b, ArithRef):
+    #     a = RealVal(a, b.ctx)
+    # if isinstance(b, float) and isinstance(a, ArithRef):
+    #     b = RealVal(b, a.ctx)
 
     s = None
     s = _coerce_expr_merge(s, a)
